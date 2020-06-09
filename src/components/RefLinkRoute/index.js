37b1a10ee28dbbf3707e0f3c797fde96558d6d03
PiotrGrzy <piotr.grzymowicz1@gmail.com>
2020-06-09 18:29:18 +0200

    change replace

--- src/components/RefLinkRoute/index.js
+++ src/components/RefLinkRoute/index.js
@@ -3,11 +3,17 @@ import { useLocation, Route, Redirect } from 'react-router-dom';
 
 const RefLinkRoute = (props) => {
   let location = useLocation();
+  //console.log(location);
   const refLink = location.search.replace('?ref=', '');
 
   if (refLink) {
+    const [first, last] = location.search.split('?');
+    const reFilterRef = /&ref(\=[^&]*)?(?=&|$)|^ref(\=[^&]*)?(&|$)/;
+    const paramsWithoutRef = last.replace(reFilterRef, '');
+    console.log(location.search);
+
     localStorage.setItem('reflink', JSON.stringify(refLink));
-    return <Redirect to={location.pathname} />;
+    return <Redirect to={location.pathname + '?' + paramsWithoutRef} />;
   }
 
   return <Route {...props} />;
